13738f7107d9aa75339734c1bf0ed419406fb348
Herry <yuandeng@aliyun.com>
2021-02-17 23:09:34 +0800

    fix bug

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -51,13 +51,19 @@
         rm -rf /data/source/nginx
     fi
 
+    if [ ! -d /data/server/nginx/conf/vhosts ]; then
+        mkdir /data/server/nginx/conf/vhosts
+    fi
+
+    if [ ! -d /data/cert ]; then
+        rm -rf /data/cert
+    fi
+
     cd /data/source
     git clone git@github.com:mucts/nginx.git nginx
     cd nginx
     mv -f nginx.conf /data/server/nginx/conf/nginx.conf
-    mkdir /data/server/nginx/conf/vhosts/
     mv -f vhosts/* /data/server/nginx/conf/vhosts/
-    mkdir /data/cert/
     mv -f cert/* /data/cert/
     rm -rf /data/source/nginx
     cd ~
