6246309bdf321d7ea3cfbd6a0f5c1d2e85a11338
Herry <yuandeng@aliyun.com>
2021-05-16 00:31:49 +0800

    pref:amqp

---
+++ config/gjk.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'url' => env('GJK_API_URL', 'https://api.guijk.com'),
+    'app_id' => env('GJK_APP_ID', 10000),
+    'secret_key' => env('GJK_SECRET_KEY', ''),
+    'version' => env('GJK_VERSION', 8000)
+];
